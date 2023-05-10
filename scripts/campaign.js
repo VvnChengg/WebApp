34528900838acd6aa0a8f411ec957e3a2f647e39
KoKoLates <the21515@gmail.com>
2023-05-10 12:09:05 +0800

    update campaign card

--- scripts/campaign.js
+++ scripts/campaign.js
@@ -10,4 +10,44 @@ document.querySelector('.campaign-form-close').addEventListener('click', () => {
     document.querySelector('.campaign-form-overlay').classList.remove('overlay-active');
 })
 
-// adding campaign 
+// query the campaign contents
+const submit_btn = document.querySelector('.campaign-form-add');
+
+submit_btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const event_name = document.querySelector('#name-input').value;
+    const event_date = document.querySelector('#date-input').value;
+    const event_time = document.querySelector('#time-input').value;
+    const event_loca = document.querySelector('#loca-input').value;
+    const event_desc = document.querySelector('#desc-input').value;
+    create_campaign({
+        'name': event_name,
+        'date': event_date,
+        'time': event_time,
+        'loca': event_loca,
+        'desc': event_desc
+    })
+
+    
+
+    document.querySelector('.campaign-form-container').classList.remove('form-active');
+    document.querySelector('.campaign-form-overlay').classList.remove('overlay-active');
+    
+});
+
+function create_campaign (event) {
+    document.querySelector('.campaign-row').insertAdjacentHTML(
+        'beforeend',
+        `<div class="campaign-col">
+            <div class="campaign-contents">
+                <span class="campaign-date">${event.date}</span>
+                <span class="campaign-name">${event.name}</span>
+                <span class="campaign-people">0 人已報名</span>
+                <div class="campaign-register">
+                    <span>報名活動</span>
+                </div>
+            </div>
+        </div>`
+    )
+}
