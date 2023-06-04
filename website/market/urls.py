9f898a2bb0daa2f9a3b0b8b3437d9d38e34b6af5
KoKoLates <the21515@gmail.com>
2023-06-04 22:09:56 +0800

    fix: market url error

--- website/market/urls.py
+++ website/market/urls.py
@@ -1,14 +1,11 @@
 from . import views
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.market_index, name='market_index'),
-    path('user/', views.user_interface, name='user'),
+    path('user_btn/', views.user_btn, name='user_btn'),
+    path('user_interface/', views.user_interface, name='user_interface'),
     path('create/', views.create, name='create'),
     path('create_interface/', views.create_interface, name='create_interface'),
-    path('<category>/', views.market_index, name='category'),
+    path('category/<str:option>/', views.category, name='category')
 ]
-
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
