8de0b1753d0ee384184d76b71ce180a8c414c55e
OSCAR WONG, WAI LOK <lokwong2001@gmail.com>
2023-06-07 09:56:35 +0800

    add chat file

---
+++ website/chat/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.apps import apps
+import uuid
+from login.models import Profile
+    
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="sender", default=1)
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="receiver", default=2)
+    content = models.TextField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.sender}: {self.content}'
+    
+class RoomDetail(models.Model):
+    room_name = models.CharField(max_length=100)
+    room_description = models.CharField(max_length=255)
+    room_capacity = models.IntegerField(default=2)
+
+    def __str__(self):
+        return self.room_name
+
+class Room(models.Model):
+    room_detail = models.ForeignKey(RoomDetail, on_delete=models.CASCADE, default=1)
+    member = models.ForeignKey(Profile, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.room_detail.room_name
---
+++ website/chat/urls.py
@@ -0,0 +1,11 @@
+# chat/urls.py
+from django.urls import path, include
+from . import views as chat_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path("", chat_views.chat_index, name="chat_index"),
+    path("<str:username>", chat_views.room, name="room"),
+    path("<str:username>/send_message", chat_views.send_message, name="send_message"),
+]
